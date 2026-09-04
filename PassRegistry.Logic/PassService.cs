9ee1949a7dc522bbcff3058aace483b12ddf5670
Ilya Kamenskiy <ilya.kamenskiy@mail.com>
2026-09-04 10:29:35 +0300

    Добавить слой логики и вывод результата

---
+++ PassRegistry.Logic/PassService.cs
@@ -0,0 +1,20 @@
+﻿using PassRegistry.Data;
+
+namespace PassRegistry.Logic;
+
+/// <summary>
+/// Сервис работы с <see cref="Pass"/>
+/// </summary>
+public sealed class PassService
+{
+    private readonly PassRepository passRepository = new();
+
+    /// <summary>
+    /// Получить список активных <see cref="Pass"/>
+    /// </summary>
+    public IReadOnlyCollection<Pass> GetActive()
+        => passRepository.GetAll()
+        .Where(x => x.IsValid)
+        .ToList()
+        .AsReadOnly();
+}
