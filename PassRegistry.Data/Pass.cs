19d528e5e04f5bb62ca2985551b19c5653cc0a4a
Ilya Kamenskiy <ilya.kamenskiy@mail.com>
2026-09-04 10:29:14 +0300

    Добавить модель и хранилище данных

---
+++ PassRegistry.Data/Pass.cs
@@ -0,0 +1,22 @@
+﻿namespace PassRegistry.Data;
+
+/// <summary>
+/// Модель сущности пропуска
+/// </summary>
+public sealed record Pass
+{
+    /// <summary>
+    /// Идентификатор
+    /// </summary>
+    public Guid Id { get; init; }
+
+    /// <summary>
+    /// Посетитель
+    /// </summary>
+    public string Visitor { get; init; } = string.Empty;
+
+    /// <summary>
+    /// Флаг актуальности
+    /// </summary>
+    public bool IsValid { get; init; }
+}
---
+++ PassRegistry.Data/PassRepository.cs
@@ -0,0 +1,31 @@
+﻿namespace PassRegistry.Data;
+
+/// <summary>
+/// Репозиторий работы с <see cref="Pass"/>
+/// </summary>
+public sealed class PassRepository
+{
+    private readonly List<Pass> items = [
+        new Pass{
+            Id = Guid.NewGuid(),
+            Visitor = "Федя",
+            IsValid = true,
+        },
+        new Pass{
+            Id = Guid.NewGuid(),
+            Visitor = "Петя",
+            IsValid = true,
+        },
+        new Pass{
+            Id = Guid.NewGuid(),
+            Visitor = "Вася",
+            IsValid = false,
+        },
+        ];
+
+    /// <summary>
+    /// Получить все сущности <see cref="Pass"/>
+    /// </summary>
+    public IReadOnlyCollection<Pass> GetAll()
+        => items.AsReadOnly();
+}
